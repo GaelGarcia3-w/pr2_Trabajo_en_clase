35aa2a510d40703646d38509949f0719c6fc1b2a
Galletas <a.23308051281182@cbtis128.edu.mx>
2024-09-24 19:52:06 -0600

    pr2_Trabajo_en_clase

---
+++ Pr2_Trabajo_en_clase.py
@@ -0,0 +1,8 @@
+print(" ")
+print("Edgar Gael Garcia Camacho :pr2_Trabajo_en_clase ")
+print(" ")
+num1=int(input("Ingresa el primer valor :")  )#Te pide que ingreses el primer valor.
+num2=int(input("Ingresa el segundo valor :") )#Te pide que ingreses el segundo valor.
+num3=int(input("Ingresa el tercer valor :") )#Te pide que ingreses el tercer valor.
+r = (num1 * num2 * num3)#se realiza la multiplicacion de los 3 valore.
+print("El resultado es :",r)#ALfinal te saltara un mensaje con el resultado.
